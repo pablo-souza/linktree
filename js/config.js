55cf0a3c594117b7e1103d3429cb65a3250ee53a
pablo-souza <souza33pablo@gmail.com>
2026-08-25 12:06:35 -0300

    config: connect published Google Sheets

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 export const APP_CONFIG = Object.freeze({
   // Substitua pelas URLs CSV das abas publicadas. Consulte o README.
-  configSheetUrl: 'URL_PUBLICA_CONFIG',
-  linksSheetUrl: 'URL_PUBLICA_LINKS',
+  configSheetUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRXfOU_9B9FWb-MY1CPLK4l6W6RRCzjQe_DDOKZFK_mZN8VpJnRzhIuipFuZ4LdNGD2JgRdhuSp3nMz/pub?gid=0&single=true&output=csv',
+  linksSheetUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRXfOU_9B9FWb-MY1CPLK4l6W6RRCzjQe_DDOKZFK_mZN8VpJnRzhIuipFuZ4LdNGD2JgRdhuSp3nMz/pub?gid=1957239952&single=true&output=csv',
 });
